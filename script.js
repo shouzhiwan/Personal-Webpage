d9fb053fa24f9efc32856cd232856b3e3d9d8aa3
Shouzhi Wan <shouzhiwan@Shouzhis-MacBook-Pro.local>
2016-08-08 23:26:19 -0700

    the outlook updated

--- script.js
+++ script.js
@@ -1,6 +1,67 @@
 $(function() {
-	setTimeout(function() {
-		$('[data-fade-in]').fadeIn(1000); 	
-	}, 1000);
-	
+	mainScreenFadeIn();
+	$('.button').click(mainScreenFadeOut);
+
+
 });
+
+var mainScreenFadeOut = function(){
+	var fileName = $(this).data('div-name');
+	sectionFadeOut();
+	if(fileName == 'Back'){
+		mainScreenFadeIn();
+	} 
+	else {
+		contentFadeIn();
+		if(fileName == 'Resume'){
+			setTimeout(function() {
+				$('.resumeContent').fadeIn(500);
+			}, 500);
+		}
+		if(fileName == 'Contacts'){
+			setTimeout(function() {
+				$('.contactContent').fadeIn(500);
+			}, 500);
+		}
+		if(fileName == 'Photos'){
+			setTimeout(function() {
+				$('.photoContent').fadeIn(500);
+			}, 500);
+		}
+		if(fileName == 'Posts'){
+			setTimeout(function() {
+				$('.postContent').fadeIn(500);
+			}, 500);
+		}
+	}
+}
+
+var mainScreenFadeIn = function(){
+	setTimeout(function() {
+		$('#fixed-button').fadeOut(500);
+		$('#main').fadeOut(500);
+	}, 0);
+
+	setTimeout(function() {
+		$('#banner').fadeIn(1000);
+		$('#button-banner').fadeIn(1000);
+	}, 500);
+
+}
+
+var contentFadeIn = function(){
+	$('#banner').fadeOut(1000);
+	$('#button-banner').fadeOut(1000);
+	setTimeout(function() {
+		$('#fixed-button').fadeIn(500);
+		$('#main').fadeIn(500);
+	}, 1000);
+}
+
+var sectionFadeOut = function(){
+	$('.resumeContent').fadeOut(500);
+	$('.photoContent').fadeOut(500);
+	$('.contactContent').fadeOut(500);
+	$('.postContent').fadeOut(500);
+
+}
